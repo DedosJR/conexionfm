2f854e482f5a7e39fb9fade4918711453d112e46
Israel <dedos_jr@hotmail.com>
2024-03-07 20:45:25 -0800

    Update grid
    update grid 7 de marzo cambios en destacado y banner de inicio

--- src/app/wordpress.service.ts
+++ src/app/wordpress.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class WordpressService {
-  private apiUrl = "http://www.conexionfm.com/wp-json/wp/v2/posts";
-  private apiDestacado = "http://www.conexionfm.com/wp-json/wp/v2/posts";
+  private apiUrl = 'http://www.conexionfm.com/wp-json/wp/v2/posts';
+  private apiDestacado = 'http://www.conexionfm.com/wp-json/wp/v2/posts';
 
   constructor(private http: HttpClient) {}
 
@@ -17,7 +17,7 @@ export class WordpressService {
     return this.http.get<any[]>(url);
   }
   getPostsd(categoryId = 107): Observable<any[]> {
-    const url = `${this.apiDestacado}?categories=${categoryId}&_embed`;
+    const url = `${this.apiDestacado}?categories=${categoryId}&per_page=1&_embed`;
     return this.http.get<any[]>(url);
   }
 }
